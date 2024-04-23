a93b086d2f1df3db12c8a064778503f3cdd78a47
Rourke Whitcombe <rourke28whit@gmail.com>
2024-04-23 01:55:19 +0000

    feat: I changed my functiuon with the mole, I wanted more holes and to better arrange the css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+let currentMoleTile;
+
 window.onload = function() {
     runGame();
 }
@@ -8,4 +10,24 @@ function runGame() {
         tile.id = i.toString();
         document.getElementById("board").appendChild(tile);
     }
+
+    setInterval(setMole, 2000);
+}
+
+function getRandomTile() {
+    let num = Math.Floor(Math.random() * 9);
+    return num.toString();
+}
+
+function setMole() {
+    if (currentMoleTile) {
+        currentMoleTile.innerHTML = "";
+    }
+
+    let mole = document.createElement("img");
+    mole.src = "assets/images/mole.png";
+
+    let num = getRandomTile();
+    currentMoleTile = document.getElementById(num);
+    currentMoleTile.appendChild(mole);
 }
