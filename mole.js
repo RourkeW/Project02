525f48fda70f47c565c1a6556d05940fa97301e4
Rourke Whitcombe <rourke28whit@gmail.com>
2024-04-23 20:33:45 +0000

    feat: initial commit today

--- mole.js
+++ mole.js
@@ -4,24 +4,21 @@ let score = 0;
 let gameOver = false;
 
 window.onload = function() {
-    setGame();
+    runGameGame();
 }
 
-function setGame() {
-    //set up the grid in html
-    for (let i = 0; i < 9; i++) { //i goes from 0 to 8, stops at 9
-        //<div id="0-8"></div>
+function runGame() {
+    for (let i = 0; i < 9; i++) { 
         let tile = document.createElement("div");
         tile.id = i.toString();
         tile.addEventListener("click", selectTile);
         document.getElementById("board").appendChild(tile);
     }
-    setInterval(setMole, 1000); // 1000 miliseconds = 1 second, every 1 second call setMole
-    setInterval(setBomb, 2000); // 2000 miliseconds = 2 seconds, every 2 second call setPlant
+    setInterval(setMole, 1000);
+    setInterval(setBomb, 2000);
 }
 
 function getRandomTile() {
-    //math.random --> 0-1 --> (0-1) * 9 = (0-9) --> round down to (0-8) integers
     let num = Math.floor(Math.random() * 9);
     return num.toString();
 }
@@ -68,10 +65,10 @@ function selectTile() {
     }
     if (this == currMoleTile) {
         score += 10;
-        document.getElementById("score").innerText = score.toString(); //update score html
+        document.getElementById("score").innerText = score.toString(); 
     }
     else if (this == currBombTile) {
-        document.getElementById("score").innerText = "GAME OVER: " + score.toString(); //update score html
+        document.getElementById("score").innerText = "GAME OVER: " + score.toString(); 
         gameOver = true;
     }
 }
